06919dccc856808f54482a2a6695254cca92efcc
quadroloop <nplixel@gmail.com>
2018-01-25 14:03:28 +0800

    working on ui

--- index.js
+++ index.js
@@ -3,30 +3,27 @@ var app     = express();
 var bodyParser = require('body-parser');
 app.use(bodyParser.urlencoded())
 app.use(bodyParser.json())
-// render HTML UI 
+// render HTML UI
 app.get('/', function (req, res) {
 res.sendFile(__dirname + '/ui/ui.html');
 });
 
 
-app.get('/updatePeople',function(){
-  console.log('update people requested');
+app.put('/updatePerson',function(req, res, next){
+  console.log(req.body)
+ res.send(200, req.body);
 });
-
-//app.get('/addPeople',function(){
-//  console.log("add people requested!");
-//});
 
 app.post('/addPeople',function(req, res, next){
    console.log(req.body)
   res.send(200, req.body);
- });   
+ });
 
 
 app.delete('/deletePerson',function(req, res, next){
    console.log(req.body)
   res.send(200, req.body);
- });   
+ });
 
 app.get('/getPeople',function(){
   console.log("get people requested");
