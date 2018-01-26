6798c27658830a2b1bd0047799f5ec9f912fe2bf
quadroloop <nplixel@gmail.com>
2018-01-26 20:17:28 +0800

    Finish Project!

--- index.js
+++ index.js
@@ -3,31 +3,63 @@ var app     = express();
 var bodyParser = require('body-parser');
 app.use(bodyParser.urlencoded())
 app.use(bodyParser.json())
-// render HTML UI
-app.get('/', function (req, res) {
-res.sendFile(__dirname + '/ui/ui.html');
+
+// connect to DB
+const { Pool, Client } = require('pg')
+
+const pool = new Pool({
+  user: 'postgres',
+  host: 'localhost',
+  database: 'users',
+  password: '',
+  port: 5433,
+})
+
+app.get('/',function(req,res,next){
+  res.sendFile(__dirname + '/ui/ui.html');
+});
+
+app.get('/getPeople',function(req, res, next){
+
+
+pool.query('SELECT * FROM user_table ORDER BY id ASC', (err, result) => {
+   res.send(result.rows);
+//  pool.end()
+})
+
 });
 
 
+// end of db
+
 app.put('/updatePerson',function(req, res, next){
+  console.log("upadating user..")
   console.log(req.body)
- res.send(200, req.body);
+// res.send(200, req.body);
+// UPDATE Customers SET ContactName = 'Alfred Schmidt', City= 'Frankfurt' WHERE CustomerID = 1;
+pool.query("UPDATE user_table SET name='"+req.body.name+"', age='"+req.body.age+"' WHERE id='"+req.body.id+"';", (err, result) => {})
 });
 
 app.post('/addPeople',function(req, res, next){
    console.log(req.body)
   res.send(200, req.body);
- });
+  pool.query("INSERT INTO user_table (name,age) VALUES ('"+req.body.name+"','"+req.body.age+"')", (err, result) => {
+ })
+
+});
+
+
 
 
 app.delete('/deletePerson',function(req, res, next){
-   console.log(req.body)
-  res.send(200, req.body);
+
+  console.log("deleteing user.." + req.body.name);
+   pool.query("DELETE FROM user_table WHERE name='"+req.body.name+"' AND age='"+req.body.age+"';", (err, result) => {
+
+   })
  });
 
-app.get('/getPeople',function(){
-  console.log("get people requested");
-});
+
 
 app.listen(3000);
 
